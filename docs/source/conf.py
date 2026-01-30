5c3dcafa9cb0466bab03250adb95ea163157bacc
Nims B <34975761+nimsb@users.noreply.github.com>
2026-01-30 18:01:54 +0100

    update naming

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Baguette'
+copyright = '2026, Nimsb'
+author = 'Nimsb'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.5'
+version = '1.5.0'
 
 # -- General configuration
 
